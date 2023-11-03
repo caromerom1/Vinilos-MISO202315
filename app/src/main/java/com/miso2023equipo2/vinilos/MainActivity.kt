261e152b26a9d8c4ff3b1bdc28ed744aedd43b6e
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-11-02 23:49:57 -0500

    feat: Include app navigation
    * Include navigation-compose dependency
    * Include HomePage, AlbumDetailPage & AlbumCataloguePage routes
    * Include TopAppBar
    * Include Application HomePage

--- app/src/main/java/com/miso2023equipo2/vinilos/MainActivity.kt
+++ app/src/main/java/com/miso2023equipo2/vinilos/MainActivity.kt
@@ -6,25 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.miso2023equipo2.vinilos.components.ListItem
-import com.miso2023equipo2.vinilos.components.VinylsList
+import androidx.compose.ui.tooling.preview.Preview
+import com.miso2023equipo2.vinilos.navigation.AppNavigation
 import com.miso2023equipo2.vinilos.ui.theme.VinilosTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val items = listOf(
-        ListItem(text = "Item 1", imageUrl = "https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg"),
-        ListItem(text = "Item 2", imageUrl = "https://i1.sndcdn.com/artworks-140a538e-5e8f-4f3c-9de0-5627dcce04b6-0-t500x500.jpg"),
-        ListItem(text = "Item 3", imageUrl = "https://hips.hearstapps.com/es.h-cdn.co/hares/images/cultura/ocio/portadas-de-discos-y-de-albums-de-musica-mas-importantes-del-s.xx/abbey-road-the-beatles-1969/4248779-1-esl-ES/abbey-road-the-beatles-1969.jpg?resize=980:*"),
-        ListItem(text = "Item 4", imageUrl = "https://hips.hearstapps.com/es.h-cdn.co/hares/images/cultura/ocio/portadas-de-discos-y-de-albums-de-musica-mas-importantes-del-s.xx/the-dark-side-of-the-moon-pink-floyd-1973/4247827-1-esl-ES/the-dark-side-of-the-moon-pink-floyd-1973.jpg?resize=980:*"),
-        ListItem(text = "Item 5", imageUrl = "https://i.pinimg.com/236x/74/a3/7d/74a37df699987b8bd8131d9dae350cd0.jpg"),
-        ListItem(text = "Item 6", imageUrl = "https://cesarmiguelrondon.b-cdn.net/wp-content/uploads/2021/03/LUNES-08-Sonido-Bestial-Ricardo-Ray-Y-Bobby-Cruz.jpg"),
-        ListItem(text = "Item 7", imageUrl = "https://musign.es/wp-content/uploads/2021/08/81SNg56GQhL._SL1425_-1-1024x1024.jpg?resize=980:*"),
-        ListItem(text = "Item 8", imageUrl = "https://cdns-images.dzcdn.net/images/cover/ee45d13546b4fbaca08564ab45218de7/264x264.jpg"),
-        ListItem(text = "Item 9", imageUrl = "https://cdns-images.dzcdn.net/images/cover/2b5c5649e9fe9a318a3c0bc49dbca9c3/264x264.jpg"),
-        ListItem(text = "Item 10", imageUrl = "https://www.lahiguera.net/musicalia/artistas/bad_bunny/disco/10439/bad_bunny_yhlqmdlg-portada.jpg")
-    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,9 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    VinylsList(items = items)
+                    AppNavigation()
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VinilosTheme {
+        AppNavigation()
     }
 }
