327bd6a9136ae556ac4cf0b961452181209c490c
Camilo Romero <ca.romerom1@uniandes.edu.co>
2023-11-04 21:58:47 -0500

    refactor: Use DetailedList component

--- app/src/main/java/com/miso2023equipo2/vinilos/pages/album/AlbumDetailPage.kt
+++ app/src/main/java/com/miso2023equipo2/vinilos/pages/album/AlbumDetailPage.kt
@@ -2,28 +2,24 @@ package com.miso2023equipo2.vinilos.pages.album
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.miso2023equipo2.vinilos.R
 import com.miso2023equipo2.vinilos.navigation.state.AlbumDetailUiState
+import com.miso2023equipo2.vinilos.ui.components.DetailedList
 import com.miso2023equipo2.vinilos.ui.components.ErrorScreen
+import com.miso2023equipo2.vinilos.ui.components.ItemDetail
 import com.miso2023equipo2.vinilos.ui.components.LoadingScreen
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
 
 @Composable
@@ -44,6 +40,12 @@ fun AlbumDetailPage(
             is AlbumDetailUiState.Success -> {
                 val album = albumDetailUiState.album
 
+                val details = listOf(
+                    ItemDetail("Nombre", album.name),
+                    ItemDetail("Descripción", album.description),
+                    ItemDetail("Fecha de lanzamiento", album.releaseDate, isDate = true),
+                    ItemDetail("Género", album.genre),
+                )
 
 
                 Column(
@@ -66,68 +68,7 @@ fun AlbumDetailPage(
                         )
                     }
                 }
-
-                Column {
-                    Row {
-                        Text(
-                            text = "Nombre", fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .width(130.dp)
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                        Text(
-                            text = album.name, modifier = Modifier
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                    }
-
-                    Row {
-                        Text(
-                            text = "Descripción", fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .width(130.dp)
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                        Text(
-                            text = album.description, modifier = Modifier
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                    }
-
-                    val formattedDate =
-                        ZonedDateTime.parse(album.releaseDate, DateTimeFormatter.ISO_DATE_TIME)
-                            .format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-                    Row {
-                        Text(
-                            text = "Fecha de \nlanzamiento", fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .width(130.dp)
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                        Text(
-                            text = formattedDate.toString(),
-                            modifier = Modifier
-                                .width(130.dp)
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                    }
-
-                    Row {
-                        Text(
-                            text = "Género", fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .width(130.dp)
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                        Text(
-                            text = album.genre, modifier = Modifier
-                                .padding(horizontal = 16.dp, vertical = 8.dp)
-                        )
-                    }
-
-                }
-
-//
+                DetailedList(details = details)
             }
 
             is AlbumDetailUiState.Error -> ErrorScreen(
