e8d3e0b353f9a87df56d4cf37584bc6961da46ae
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2023-11-05 16:35:24 -0500

    test: Fix labels and harcoded elements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,10 +76,12 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("io.coil-kt:coil-compose:2.4.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/androidTest/java/com/miso2023equipo2/vinilos/VynilsScreenNavigationTest.kt
+++ app/src/androidTest/java/com/miso2023equipo2/vinilos/VynilsScreenNavigationTest.kt
@@ -2,17 +2,32 @@ package com.miso2023equipo2.vinilos
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onLast
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import com.miso2023equipo2.vinilos.navigation.AppNavigation
 import com.miso2023equipo2.vinilos.navigation.AppPages
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+
 class VynilsScreenNavigationTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
@@ -42,9 +57,14 @@ class VynilsScreenNavigationTest {
         navigateToAlbumCatalogueScreen()
         navController.assertCurrentRouteName(AppPages.AlbumCataloguePage.route)
     }
+
     private fun navigateToAlbumCatalogueScreen(){
         composeTestRule.onNodeWithStringId(R.string.collectionist_label)
             .performClick()
     }
+
+
+
+
 }
 
